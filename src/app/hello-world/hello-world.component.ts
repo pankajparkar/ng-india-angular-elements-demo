78570eb021273d26ea1f0f62058f76015cecb31f
Pankaj Parkar <parkarpankaj3@gmail.com>
2018-03-03 14:15:06 +0530

    Placed component inside an outside Angular.

--- src/app/hello-world/hello-world.component.ts
+++ src/app/hello-world/hello-world.component.ts
@@ -4,7 +4,7 @@ import { Component, OnInit, Input, ViewEncapsulation } from '@angular/core';
   selector: 'app-hello-world',
   templateUrl: './hello-world.component.html',
   styleUrls: ['./hello-world.component.css'],
-  encapsulation: ViewEncapsulation.Native
+  // encapsulation: ViewEncapsulation.Native
 })
 export class HelloWorldComponent implements OnInit {
   @Input() greetings: string;
